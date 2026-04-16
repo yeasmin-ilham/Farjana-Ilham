dbfebdbf33b3ae77461919de5c2462558c6a952f
Yeasmin Ilham <farjanayeasminilham@gmail.com>
2026-04-16 13:45:02 +0600

    initial setup done

---
+++ app/components/HomePage.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image"
+
+
+export default function HomePage(){
+    return(
+      <div>
+          <section className="min-h-screen max-w-7xl mx-auto bg-white ">
+             <div>
+                hello
+             </div>
+        </section>
+      </div>
+       
+        
+    )
+}
